c0c63a440aa16a28d325162c1c292321e9348f65
hcs <hannahemcs@gmail.com>
2023-11-10 09:26:17 +0100

    Break out into more specific classes

--- src/main/java/org/example/UserManagement/User.java
+++ src/main/java/org/example/UserManagement/User.java
@@ -4,26 +4,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    private String username;
-    private String password;
-    private UserRole userRole;
+    public class UserInfo {
+        private String username;
+        private String password;
+        private UserRole userRole;
+
+        public User(String username, String password, UserRole userRole) {
+            this.username = username;
+            this.password = password;
+            this.userRole = userRole;
+            UserLists.allUsers.add(this);
+            switch (userRole){
+                case (UserRole.ADMIN) : UserLists.admins.add(this);
+                    break;
+                case (UserRole.TEACHER) : UserLists.teachers.add(this);
+                    break;
+                case (UserRole.STUDENT) : UserLists.students.add(this);
+                    break;
+                default :
+            }
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public UserRole getUserRole() {
+            return userRole;
+        }
+
+        public void setUserRole(UserRole userRole) {
+            this.userRole = userRole;
+        }
+    }
+
+    public class UserLists {
     private static List<User> allUsers = new ArrayList<>();
     private static List<User> students = new ArrayList<>();
     private static List<User> teachers = new ArrayList<>();
     private static List<User> admins = new ArrayList<>();
-    public User(String username, String password, UserRole userRole) {
-        this.username = username;
-        this.password = password;
-        this.userRole = userRole;
-        allUsers.add(this);
-        switch (userRole){
-            case (UserRole.ADMIN) : admins.add(this);
-            break;
-            case (UserRole.TEACHER) : teachers.add(this);
-            break;
-            case (UserRole.STUDENT) : students.add(this);
-            break;
-            default :
-        }
     }
 }
