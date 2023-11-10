309e2992c0c0607104c52ce46952199bb929fde4
hcs <hannahemcs@gmail.com>
2023-11-10 10:29:57 +0100

    Create course class

---
+++ src/main/java/org/example/CourseManagement/Course.java
@@ -0,0 +1,51 @@
+package org.example.CourseManagement;
+import org.example.UserManagement.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Course {
+    // properties
+    private String name;
+    private static int uid = 0;
+    private int id;
+    private static List<Course> allCourses = new ArrayList<>();
+    private List<Student> enrolledStudents = new ArrayList<>();
+    private Map<Student, String> grades = new HashMap<>();
+
+    //constructor
+    public Course(String name) {
+        this.name = name;
+        this.id = uid++;
+        allCourses.add(this);
+    }
+
+    // getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //CRUD
+    public void delete(Course course){
+        allCourses.remove(course);
+    }
+
+    public void update(Course course, String name){
+        this.name = name;
+    }
+}
--- src/main/java/org/example/UserManagement/Student.java
+++ src/main/java/org/example/UserManagement/Student.java
@@ -1,4 +1,5 @@
 package org.example.UserManagement;
+import org.example.CourseManagement.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
