3811a76e6d5ba01a060355a1e8ed4da2f55a76a3
hcs <hannahemcs@gmail.com>
2023-11-10 15:20:42 +0100

    Create student menu and register student sub menu

---
+++ src/main/java/org/example/Menu/StudentSubMenu/StudentRegMenu.java
@@ -0,0 +1,29 @@
+package org.example.Menu.StudentSubMenu;
+import org.example.UserManagement.*;
+
+import java.util.Scanner;
+
+public class StudentRegMenu {
+    public void run() {
+        boolean exitLoop = false;
+        while (!exitLoop) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Register new student.");
+            System.out.println("Create login details:");
+            System.out.println("Create student username");
+            String studentUsername = scanner.nextLine();
+            System.out.println("Create student password");
+            String studentPW = scanner.nextLine();
+            User user = new User(studentUsername, studentPW, UserRole.STUDENT);
+            int studentUserId = user.getId();
+            System.out.println("Enter student full name");
+            String studentName = scanner.nextLine();
+            Student student = new Student(studentName, studentUserId);
+            System.out.println(studentName+" has been registered. Would you like to register another student? (y/n)");
+            String goAgain = scanner.nextLine();
+            if (goAgain.equals("n") || goAgain.equals("N")) {
+                exitLoop = true;
+            }
+        }
+    }
+}
